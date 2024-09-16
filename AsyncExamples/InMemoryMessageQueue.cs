d09f7af983db89689843c8dea5a1d37a1d18a0e9
kapitalrisk <nathan.quintard@gmail.com>
2024-09-16 11:36:57 +0200

    Add basic message queue example

---
+++ AsyncExamples/InMemoryMessageQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeExamples
+{
+    // This is supposed to be action performed on a remote message queue service
+    internal class InMemoryMessageQueue
+    {
+        private ConcurrentQueue<string> _messageQueue = new ConcurrentQueue<string>();
+
+        public bool PushMessage(string message)
+        {
+            _messageQueue.Append(message);
+            return true;
+        }
+
+        public string? PullMessage()
+        {
+            var peek = _messageQueue.TryPeek(out var result);
+
+            return peek ? result : null;
+        }
+
+        public bool AcknowledgeLastMessage() => _messageQueue.TryDequeue(out var _);
+
+        public bool IsMessageAvailable => _messageQueue.TryPeek(out var _);
+    }
+}
---
+++ AsyncExamples/MessageQueueExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeExamples
+{
+    internal class MessageQueueExample
+    {
+        public void MessageQueueExample1()
+        {
+            var mqs = new InMemoryMessageQueue();
+
+            mqs.PushMessage("hello");
+            mqs.PushMessage("world");
+
+            while (mqs.IsMessageAvailable)
+            {
+                var message = mqs.PullMessage();
+                Console.Write(message);
+                Console.Write(Environment.NewLine);
+                mqs.AcknowledgeLastMessage();
+            }
+
+            // Will display "hello\nworld"
+        }
+
+        public void MessageQueueExample2()
+        {
+            var mqs = new InMemoryMessageQueue();
+
+            mqs.PushMessage("hello");
+            mqs.PushMessage("world");
+
+            while (mqs.IsMessageAvailable)
+            {
+                var message = mqs.PullMessage();
+                Console.Write(message);
+                Console.Write(Environment.NewLine);
+            }
+
+            // Infinite loop that will display "hello" multiple times before crash
+        }
+    }
+}
